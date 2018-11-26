ae8afe0aa1705e7423b00cfe3a44507fa0d55b92
Rafal Ozog <rafalozog@gmail.com>
2018-11-26 00:01:01 +0000

    UI changes, animations implementation

--- app/src/main/java/development/dreamcatcher/muzmatchlight/Models/Message.kt
+++ app/src/main/java/development/dreamcatcher/muzmatchlight/Models/Message.kt
@@ -1,14 +1,6 @@
 package development.dreamcatcher.muzmatchlight.Models
 
-class Message {
-
-    val id: Int
-    var messageText: String? = null
-    val isOwnMessage: Boolean
-
-    constructor(id: Int, messageText: String, isOwnMessage: Boolean) {
-        this.id = id
-        this.messageText = messageText
-        this.isOwnMessage = isOwnMessage
-    }
-}
+class Message( val id: Int,
+               var messageText: String? = null,
+               val timestamp: String,
+               val isOwnMessage: Boolean)
