b7882f092868c6b66356d307835431bf09c1f455
Rafal Ozog <rafalozog@gmail.com>
2018-11-26 19:30:03 +0000

    Repo reorganization

---
+++ Source Code/app/src/main/java/development/dreamcatcher/muzmatchlight/ViewModels/ChatViewModel.kt
@@ -0,0 +1,10 @@
+package development.dreamcatcher.muzmatchlight.ViewModels
+
+import android.arch.lifecycle.ViewModel
+
+
+class ChatViewModel : ViewModel() {
+
+    // Not implemented yet. The business logic and data will be eventually provided through
+    // the ViewModel to separate them from the View layer.
+}
